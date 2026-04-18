94bb7d9b47d018acb959676a7e3467bf988343fd
darkwingdck <darkwingdck0@gmail.com>
2026-04-19 00:30:54 +0300

    grab ring mvp

--- Scripts/ray_cast_3d.gd
+++ Scripts/ray_cast_3d.gd
@@ -2,15 +2,27 @@ extends RayCast3D
 
 @export var pull_speed: float = 12.0
 @export var stop_distance: float = 2.2
+@export var grab_ring_hint_scene: PackedScene
 
 var grabbed_mob: CharacterBody3D = null
+var grab_ring: Node3D = null
+var grab_ring_hint: Node3D = null
+
+func _ready() -> void:
+	grab_ring = get_node("../GrabRing")
+	grab_ring_hint = grab_ring_hint_scene.instantiate()
+	grab_ring_hint.visible = false
+	grab_ring.add_child(grab_ring_hint, true)
 
 func _physics_process(delta: float) -> void:
 	if !is_colliding():
+		grab_ring_hint.visible = false
 		return
 	var hit: Object = get_collider()
 	if "Mob" not in hit.name:
+		grab_ring_hint.visible = false
 		return
+	grab_ring_hint.visible = true
 	grabbed_mob = hit
 	
 	if Input.is_action_pressed("grab"):
